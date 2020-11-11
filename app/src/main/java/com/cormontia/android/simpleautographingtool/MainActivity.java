207708608e812ddf071737db73f22f84b82562d1
FractionalRadix <nerostjust@gmail.com>
2020-11-11 22:03:54 +0100

    Completed the "happy path" of the loading function.

--- app/src/main/java/com/cormontia/android/simpleautographingtool/MainActivity.java
+++ app/src/main/java/com/cormontia/android/simpleautographingtool/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -24,6 +25,8 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity implements ListOfPoints {
+
+    private static final String TAG = "SimpleAutographTool";
 
     private AutographViewModel viewModel;
 
@@ -70,9 +73,6 @@ public class MainActivity extends AppCompatActivity implements ListOfPoints {
         } else if (itemId == R.id.share) {
             String svg = buildSvg();
 
-            //TODO!- for debugging
-            Toast.makeText(this, svg, Toast.LENGTH_LONG).show();
-
             Intent share = new Intent(Intent.ACTION_SEND);
             //TODO?~ Is this the right type for the Intent...?
             share.setType("image/svg+xml");
@@ -105,10 +105,51 @@ public class MainActivity extends AppCompatActivity implements ListOfPoints {
 
     private void loadAutograph() {
         String filename = "autograph.svg";
+        String svg = readFile(filename);
+        if (svg == null) {
+            return;
+        }
 
+        Toast.makeText(this, svg, Toast.LENGTH_LONG).show();
+        Log.i(TAG, svg);
+        int pointsAttributeIdx = svg.indexOf("points=\"");
+        int pointsListStartIdx = pointsAttributeIdx + "points=\"".length();
+        int pointsListEndIdx = svg.indexOf("\"", pointsListStartIdx);
+        Log.i(TAG, pointsAttributeIdx +  " " + pointsListStartIdx + " " + pointsListEndIdx);
+        String pointsList = svg.substring(pointsListStartIdx, pointsListEndIdx);
 
+        //TODO!- FOR DEBUGGING
+        Toast.makeText(this, pointsList, Toast.LENGTH_LONG).show();
+
+        // "pointsList" now consists of a list of coordinates.
+        // Separated by whitespace, each pair of coordinates is of the form "x,y", where x and y are integers.
+        String[] arrayOfCoordinates = pointsList.split(" ");
+        for (String currentCoordinates : arrayOfCoordinates) {
+            // It is possible that some of the elements in the array are just whitespace. Let's avoid that.
+            Log.i(TAG, "coordinates: [" + currentCoordinates +"]");
+            String trimmedCoordinates = currentCoordinates.trim();
+            if (trimmedCoordinates.length() > 0)
+            {
+                String[] coordinatePair = trimmedCoordinates.split(",");
+                //TODO!+ Handle the case that coordinatePair.length != 2
+                int x = Integer.parseInt(coordinatePair[0]); //TODO!+ Handle parser failure
+                int y = Integer.parseInt(coordinatePair[1]); //TODO!+ Handle parser failure
+                addPoint(new Point(x,y)); //TODO?~ Make a list first, and only if everything goes right, display it?
+                //TODO!+ After all points are added, force a refresh ?
+            }
+        }
+
+    }
+
+    //TODO?~ Use an Optional?
+    /**
+     * Read the text file with the given name into a String.
+     * @param filename Name of the text file.
+     * @return The contents of the text file, or <code>null</code> if something went wrong.
+     */
+    private String readFile(String filename) {
+        StringBuilder res = new StringBuilder();
         try (FileInputStream fis = this.openFileInput(filename)) {
-            StringBuilder res = new StringBuilder();
             int ch;
             while (true){
                 ch = fis.read();
@@ -120,14 +161,16 @@ public class MainActivity extends AppCompatActivity implements ListOfPoints {
             //TODO!- FOR TESTING
             Toast.makeText(this, res, Toast.LENGTH_LONG).show();
 
+            return res.toString();
         } catch (FileNotFoundException fnfe) {
             Toast.makeText(this, "Cannot open file \"" + filename + "\"", Toast.LENGTH_LONG).show();
             fnfe.printStackTrace();
+            return null;
         } catch (IOException ioe) {
             Toast.makeText(this, "Unable to load file \"" + filename + "\".", Toast.LENGTH_LONG).show();
             ioe.printStackTrace();
+            return null;
         }
-
     }
 
 
